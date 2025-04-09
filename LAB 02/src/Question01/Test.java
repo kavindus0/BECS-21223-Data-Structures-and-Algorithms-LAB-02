6a6936fdb28cdb23b32fc4f080228d7771019b4e
Kavindu Sachinthe <126804361+kavindus0@users.noreply.github.com>
2025-04-09 11:02:13 +0530

    Initial Commitment

---
+++ LAB 02/src/Question01/Test.java
@@ -0,0 +1,8 @@
+package Question01;
+
+public class Test {
+    public static void main(String[] args) {
+        StringConcator str01 = new StringConcator("Data","Structure");
+        str01.StringConcatMethod();
+    }
+}
---
+++ LAB 02/src/Question02/Queue.java
@@ -0,0 +1,4 @@
+package Question02;
+
+public class Queue {
+}
